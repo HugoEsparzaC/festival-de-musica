59cff481a63ce2ba669dc34e4cd0161345052a2d
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2024-06-22 00:29:53 -0600

    add: scroll

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
     navegacionFija()
     crearGaleria()
+    resaltarEnlace()
+    scrollNav()
 })
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+    })
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section')
+        const navlinks = document.querySelectorAll('.navegacion-principal a')
+        let actual = ''
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop
+            const sectionHight = section.clientHeight
+            if(window.scrollY >= (sectionTop - sectionHight / 3)) {
+                actual = section.id
+            }
+        })
+        navlinks.forEach(link => {
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active')
+            }
+        })
+    })
+}
 
 function navegacionFija() {
     const header = document.querySelector('.header')
